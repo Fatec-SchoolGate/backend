7244baaf6c9cc4aceea05aed8127bc036d2e0260
RO03M <romera.gv.s@gmail.com>
2023-08-23 21:41:33 -0300

    fix: typo

--- src/database/migrations/20230822222813-create_organization_subjects_table.js
+++ src/database/migrations/20230822222813-create_organization_subjects_table.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable("organization_subjectss", {
+    await queryInterface.createTable("organization_subjects", {
       id: {
         type: Sequelize.UUID,
         unique: true,

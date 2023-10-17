2258a6cac07e091ccc40f9709b1b28425aa5903c
gabriel.romera <gabriel.romera@hit.com.vc>
2023-10-17 19:55:45 -0300

    feat: multiple image uploads

--- src/firebase_storage/firebase_storage.service.ts
+++ src/firebase_storage/firebase_storage.service.ts
@@ -20,4 +20,29 @@ export class FirebaseStorageService {
 
         return publicUrl;
     }
+
+    public async uploadImages(images: Express.Multer.File[]) {
+        const bucket = firebaseAdmin.storage().bucket();
+
+        const publicUrls: string[] = [];
+
+        for (let i = 0; i < images.length; i++) {
+            const image = images[i];
+            const imageHash = createHash("sha1").update(image.buffer).digest("hex");
+        
+            const storageFilePath = `${imageHash}`;
+            const fileRef = bucket.file(storageFilePath);
+            await fileRef.save(image.buffer, {
+                metadata: {
+                    contentType: image.mimetype
+                }
+            });
+
+            const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${storageFilePath}`;
+
+            publicUrls.push(publicUrl);
+        };
+        
+        return publicUrls;
+    }
 }
